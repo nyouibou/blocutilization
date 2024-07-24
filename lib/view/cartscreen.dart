5d789717818ab1af4e7c635fa42b69f5a2385797
nyouibou <akshaits4@gmail.com>
2024-07-24 16:06:40 +0530

    betaupdates

--- lib/view/cartscreen.dart
+++ lib/view/cartscreen.dart
@@ -25,57 +25,61 @@ class MyCart extends StatelessWidget {
             itemCount: state.clist!.length,
             itemBuilder: (BuildContext context, int index) {
               Carts c = state.clist![index];
-              return Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Cart ID: ${c.id}"),
-                    Text("User ID: ${c.userId}"),
-                    Text("Total Quantity: ${c.totalQuantity}"),
-                    Text("Total Products: ${c.totalProducts}"),
-                    Text("Discounted Total: ${c.discountedTotal}"),
-                    SizedBox(height: 10),
-                    if (c.products != null && c.products!.isNotEmpty)
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: c.products!.map((product) {
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    border: Border.all()),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      height: 100,
-                                      width: 100,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              image: NetworkImage(
-                                                  "${product.thumbnail}"))),
-                                    ),
-                                    Text("Product ID: ${product.id}"),
-                                    Text("Product Name: ${product.title}"),
-                                    Text("Quantity: ${product.quantity}"),
-                                    Text("Price: ${product.price}"),
-                                  ],
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Cart ID: ${c.id}"),
+                      Text("User ID: ${c.userId}"),
+                      Text("Total Quantity: ${c.totalQuantity}"),
+                      Text("Total Products: ${c.totalProducts}"),
+                      Text("Discounted Total: ${c.discountedTotal}"),
+                      SizedBox(height: 10),
+                      if (c.products != null && c.products!.isNotEmpty)
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: c.products!.map((product) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all()),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        height: 100,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    "${product.thumbnail}"))),
+                                      ),
+                                      Text("Product ID: ${product.id}"),
+                                      Text("Product Name: ${product.title}"),
+                                      Text("Quantity: ${product.quantity}"),
+                                      Text("Price: ${product.price}"),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            );
-                          }).toList(),
+                              );
+                            }).toList(),
+                          ),
                         ),
-                      ),
-                    if (c.products == null || c.products!.isEmpty)
-                      Text('No products in this cart'),
-                  ],
+                      if (c.products == null || c.products!.isEmpty)
+                        Text('No products in this cart'),
+                    ],
+                  ),
                 ),
               );
             },
